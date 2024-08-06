4ed99a593940e8edd7e23d08e47c9537b40382c9
phanbadu <duphan03@gmail.com>
2024-08-07 05:22:22 +0700

    start npm init

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,11 +1,13 @@
 import AccountUser from '../components/AccountUser';
 import Posts from '../components/Posts';
+import CreatePost from '../components/CreatePost';
 
 export default function Account() {
     return (
         <div className="flex-1 flex flex-col gap-4">
             <AccountUser />
             <Posts />
+            <CreatePost />
         </div>
     );
 }
